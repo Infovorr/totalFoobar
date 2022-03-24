ae5d3f7570d4c62ad15deff0a9d93c86637d2f30
Val <infovorr@gmail.com>
2022-03-24 02:42:35 -0400

    Create hyILreadyDdTht.py

---
+++ hyILreadyDdTht.py
@@ -0,0 +1,26 @@
+def hyILreadyDdTht(n, b):
+	k = len(n)
+	x = [i for i in str(n)]
+	pastValues = [str(n)]
+	while (1):
+		x.sort(reverse = True)
+		y = x[:]
+		y.sort()
+		xNum = int(''.join(x), b)
+		yNum = int(''.join(y), b)
+		zNum = xNum - yNum
+		time.sleep(5)
+		if (b != 10):
+			newZNum = ''
+			while zNum:
+				newZNum = str(zNum % b) + newZNum
+				zNum //= b
+			zNum = newZNum
+		x = [i for i in str(zNum)]
+		if (len(x) < k):
+			x = (['0'] * (k - len(x))) + x
+		xTemp = ''.join(x)
+		if (xTemp in pastValues):
+			distance = len(pastValues) - pastValues.index(xTemp)
+			return distance
+		pastValues.append(xTemp)
